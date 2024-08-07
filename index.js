d31e4427d61778898c4e84809894b629d4102d86
Nariman Suleimenov <162845160+NarSulEsz@users.noreply.github.com>
2024-08-06 23:26:10 -0400

    New instance of Pool object. Connecting the js with database

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 // Import required modules
 const inquirer = require('inquirer');
 const { Pool } = require('pg');
+
+// New instance of Pool object. Connecting the js with database
+const pool = new Pool({
+    user: 'postgres', 
+    host: 'localhost',
+    database: 'staff', 
+    password: 'Tu154', 
+    port: 5432,
+  });
